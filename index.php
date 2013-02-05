cccea5ab236646528a060433d0b5372ff9e8cdc8
andkom <andkom@users.noreply.github.com>
2013-02-05 19:50:07 +0200

    home page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>GM (Geiger-Muller) Tubes Database</title>
+    <meta name="keywords" content="gm tube geiger muller database info datasheet radiation radioactive" />
+    <meta name="description" content="GM (Geiger Muller) database. Datasheets and information." />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="lib/style.css" />
+    <script type="text/javascript" src="lib/jquery.js"></script>
+    <script type="text/javascript" src="lib/calc.js"></script>
+</head>
+    <body>
+        <h1>GM Tube Database</h1>
+        <p>Site is under construction.</p>
+        <h1>Tools</h1>
+        <ul>
+            <li><a href="/viewer/">GMC-300 Online Log Viewer</a></li>
+            <li><a href="/calc/">GM Tube Conversion Factor Calculator</a></li>
+        </ul>
+    </body>
+</html>
