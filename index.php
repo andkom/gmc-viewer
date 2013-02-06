205592a58303031a5b4617277b2dbfa11f94642a
andkom <andkom@users.noreply.github.com>
2013-02-06 14:15:31 +0200

    descr change

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <title>GM (Geiger-Muller) Tube Database</title>
     <meta name="keywords" content="gm tube geiger muller database info datasheet radiation radioactive" />
-    <meta name="description" content="GM (Geiger Muller) database. Datasheets and information." />
+    <meta name="description" content="GM (Geiger Muller) database. Datasheets and information. SBM-20, SBT-9, LND 712, LND 7317 and others." />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <link rel="stylesheet" type="text/css" href="lib/style.css" />
     <script type="text/javascript" src="lib/jquery.js"></script>
