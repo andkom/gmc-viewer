dc411d5440521364189329587042eecaa4ddab5a
andkom <andkom@users.noreply.github.com>
2013-03-07 11:48:38 +0200

    home updated

--- index.php
+++ index.php
@@ -6,17 +6,22 @@
     <meta name="description" content="GM (Geiger Muller) tube database. Datasheets and information. SBM-20, SBT-9, LND 712, LND 7317 and others." />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <link rel="stylesheet" type="text/css" href="lib/style.css" />
-    <script type="text/javascript" src="lib/jquery.js"></script>
-    <script type="text/javascript" src="lib/calc.js"></script>
 </head>
     <body>
         <h1>GM Tube Database</h1>
         <p>Site is under construction.</p>
-        <h1>Tools</h1>
+        <h2>Tools</h2>
         <ul>
             <li><a href="/viewer/">GMC-300 Online Log Viewer</a></li>
             <li><a href="/calc/">GM Tube Conversion Factor Calculator</a></li>
             <li><a href="/viewer/view.php?n=chernobyl">Radiation levels during two-day trip to Chernobyl Exclusion Zone</a></li>
+        </ul>
+        <h2>Links</h2>
+        <ul>
+            <li><a href="http://www.gqelectronicsllc.com/comersus/store/comersus_viewItem.asp?idProduct=4542">GQ Electronics GMC-300 Geiger Counter</a></li>
+            <li><a href="https://sites.google.com/site/diygeigercounter/">DIYGeigerCounter</a></li>
+            <li><a href="https://github.com/andkom/zxcounter">Alternative software for the DIYGeigerCounter</a></li>
+            <li><a href="https://sites.google.com/site/geigerbot/">Geiger Bot</a></li>
         </ul>
     </body>
 </html>
